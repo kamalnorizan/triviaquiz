a1fe1b2485ed3a940a6e7f5495faa238bb23a173
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2021-03-16 10:19:04 +0800

    question

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-// void main() {
-//   runApp(MyApp());
-// }
+var questions = [
+  'What\'s your favorite color?',
+  'What\'s your favorite animal?',
+  'What\'s your favorite mall?',
+  'Where\'s your favorite place?',
+];
 
 class MyApp extends StatelessWidget {
   @override
@@ -19,22 +22,36 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('Soalan Quiz!'),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
+              child: Center(
+                child: Text(
+                  questions[2],
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+            ),
             ElevatedButton(
-              onPressed: null,
+              onPressed: answerQuestion,
               child: Text('Answer 1'),
             ),
             ElevatedButton(
-              onPressed: null,
+              onPressed: () => print('Answer 2 chosen!'),
               child: Text('Answer 2'),
             ),
             ElevatedButton(
-              onPressed: null,
+              onPressed: () {
+                print('Answer 3 Chosen!');
+              },
               child: Text('Answer 3'),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void answerQuestion() {
+    print('You choose Answer 1');
   }
 }
