0bb2d0d87b7488e577e75d513df76b90e012c6fe
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2021-03-16 12:28:30 +0800

    questions in object

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,22 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   var _questions = [
-    'What\'s your favorite color?',
-    'What\'s your favorite animal?',
-    'What\'s your favorite mall?',
-    'Where\'s your favorite place?',
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Cat', 'Fish', 'Lion', 'Tiger'],
+    },
+    {
+      'questionText': 'What\'s your favorite mall?',
+      'answers': ['Ioi Mall', 'Alamanda', 'AEON', 'Giant Hypermarket'],
+    },
+    {
+      'questionText': 'Where\'s your favorite place?',
+      'answers': ['Home', 'Work', 'Australia', 'Singapore'],
+    },
   ];
 
   void _answerQuestion() {
@@ -38,7 +50,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(_questions[_questionIndex]),
+            Question(_questions[_questionIndex]['questionText']),
             Buttons(_answerQuestion),
           ],
         ),
